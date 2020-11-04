266164744907a1e69c1f2b95036f70fd47aac95f
zitaretz <zita.aretz@gmail.com>
2020-11-04 12:52:58 -0500

    vers

--- excel.R
+++ excel.R
@@ -1,3 +1,7 @@
 #get the excel files 
 #go to specific sheet get values, remove duplicates, 
 #order by log prob, get rid of modifications and .
+install.packages("tidyverse")
+install.packages("readxl")
+# install.packages("devtools")
+devtools::install_github("tidyverse/readxl")
